ae6d2ef23d4221294548ea9ba9654d6bee442517
viktoranastasov92 <viktor.anastasov92@gmail.com>
2026-06-09 22:03:34 +0300

    Include example hello world source files

---
+++ src/test/java/com/example/service_dependency_tracker/ServiceDependencyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.service_dependency_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDependencyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
